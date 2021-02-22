90f814846e305d90ed9f2a0d0660f8f49db84be6
Vovan1608 <vovan1608@gmail.com>
2021-02-22 16:01:59 +0200

    added variables

--- #2_isLucky/main.js
+++ #2_isLucky/main.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
 
 Given a ticket number n, determine if it's lucky or not.
@@ -11,4 +11,13 @@ For n = 239017, the output should be
 isLucky(n) = false.
 */
 
-function isLucky(n) {}
+function isLucky(n) {
+  let sumOfFirstHalf = 0;
+  let sumOfSecondHalf = 0;
+  let halfOfNum = String(n).length / 2;
+
+  //   console.log(halfOfNum);
+  return;
+}
+
+isLucky(1230);
