fb8b4f5b6748f63d782a34a966aa7372dd6dae89
Vovan1608 <vovan1608@gmail.com>
2021-02-24 17:31:11 +0200

    refactoring

--- #6_Add_Border/main.js
+++ #6_Add_Border/main.js
@@ -2,18 +2,25 @@
 Given a rectangular matrix of characters, add a border of asterisks(*) to it.
 Example
 For
-picture = ["abc",
-        "ded"]
+picture = [
+  "abc",
+  "ded"
+]
 the output should be
-addBorder(picture) = ["*****",
-                    "*abc*",
-                    "*ded*",
-                    "*****"]
+addBorder(picture) = [
+  "*****",
+  "*abc*",
+  "*ded*",
+  "*****"
+  ]
 */
 
 function addBorder(picture) {
-  const widthElem = "*".repeat(picture[0].length + 2);
-  return [widthElem, ...picture.map((el) => `*${el}*`), widthElem];
+  return [
+    (widthElem = "*".repeat(picture[0].length + 2)),
+    ...picture.map((el) => `*${el}*`),
+    widthElem,
+  ];
 }
 
 const picture = ["abc", "ded"];
