9f31af98bf1c11741b0cc3e87d450e2cd1694b77
Vovan1608 <vovan1608@gmail.com>
2021-03-01 17:57:55 +0200

    done #9

--- #9_palindromeRearranging/main.js
+++ #9_palindromeRearranging/main.js
@@ -7,7 +7,15 @@ palindromeRearranging(inputString) = true.
 We can rearrange "aabb" to make "abba", which is a palindrome.
 */
 
-function palindromeRearranging(inputString) {}
+function palindromeRearranging(inputString) {
+  return (
+    inputString
+      .split("")
+      .sort()
+      .join("")
+      .replace(/(\w)\1/g, "").length < 2
+  );
+}
 
 const inputString = "aabb";
 const res = palindromeRearranging(inputString);
