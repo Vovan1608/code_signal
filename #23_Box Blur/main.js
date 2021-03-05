cc0b9dfa7df5ceacb36902279036b5a50b24803b
Vovan1608 <vovan1608@gmail.com>
2021-03-05 21:52:56 +0200

    adedd image, res

--- #23_Box Blur/main.js
+++ #23_Box Blur/main.js
@@ -4,6 +4,23 @@ Last night you partied a little too hard. Now there's a black and white photo of
 The pixels in the input image are represented as integers. The algorithm distorts the input image in the following way: Every pixel x in the output image has a value equal to the average value of the pixel values from the 3 × 3 square that has its center at x, including x itself. All the pixels on the border of x are then removed.
 
 Return the blurred image as an integer, with the fractions rounded down.
+
+For
+
+image = [[1, 1, 1], 
+         [1, 7, 1], 
+         [1, 1, 1]]
+the output should be boxBlur(image) = [[1]].
+
+To get the value of the middle pixel in the input 3 × 3 square: (1 + 1 + 1 + 1 + 7 + 1 + 1 + 1 + 1) = 15 / 9 = 1.66666 = 1. The border pixels are cropped from the final result.
 */
 
 function boxBlur(image) {}
+
+const image = [
+  [1, 1, 1],
+  [1, 7, 1],
+  [1, 1, 1],
+];
+const res = boxBlur(image);
+console.log(res);
