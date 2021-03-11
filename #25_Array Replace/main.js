fa1ef64c15d9b29625e645c11c4f03980978b090
Vovan1608 <vovan1608@gmail.com>
2021-03-11 10:45:34 +0200

    added function

--- #25_Array Replace/main.js
+++ #25_Array Replace/main.js
@@ -6,3 +6,15 @@ Example
 For inputArray = [1, 2, 1], elemToReplace = 1, and substitutionElem = 3, the output should be
 arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
  */
+
+function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+  return inputArray;
+}
+
+const inputArray = [1, 2, 1];
+const elemToReplace = 1;
+const substitutionElem = 3;
+
+const res = arrayReplace(inputArray, elemToReplace, substitutionElem);
+
+document.writeln(res);
