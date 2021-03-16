4277ff22ad40f997e64763ef55fbc39c06a7a828
Vovan1608 <vovan1608@gmail.com>
2021-03-16 10:21:54 +0200

    added div class='add', function chess...

---
+++ #29_chessBoardCellColor/main.js
@@ -0,0 +1,12 @@
+/* 
+Given two cells on the standard chess board, determine whether they have the same color or not.
+Example
+For cell1 = "A1" and cell2 = "C3", the output should be
+chessBoardCellColor(cell1, cell2) = true.
+ */
+
+function chessBoardCellColor(cell1, cell2) {}
+
+const cell1 = "A1";
+const cell2 = "C3";
+const res = chessBoardCellColor(cell1, cell2);
