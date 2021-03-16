2344cd8406a0949ecf3fed3cd772d3c3dc829180
Vovan1608 <vovan1608@gmail.com>
2021-03-16 15:31:46 +0200

    done #29

--- #29_chessBoardCellColor/main.js
+++ #29_chessBoardCellColor/main.js
@@ -1,18 +1,27 @@
-/* 
+/*
 Given two cells on the standard chess board, determine whether they have the same color or not.
 Example
 For cell1 = "A1" and cell2 = "C3", the output should be
 chessBoardCellColor(cell1, cell2) = true.
  */
 
-function chessBoardCellColor(cell1, cell2) {}
+function chessBoardCellColor(cell1, cell2) {
+  return getCell(cell1) === getCell(cell2);
+}
 
 function getCell(cell) {
-  const blackFigures = [
-    ["A", "C", "E", "G"],
-    [1, 3, 5, 7],
-  ];
+  const blackFiguresEven = ["A", "C", "E", "G"];
+
+  const blackFiguresOdd = ["B", "D", "F", "H"];
+
+  const [letter, number] = [...cell];
+
+  return (blackFiguresEven.includes(letter) && number % 2 !== 0) ||
+    (blackFiguresOdd.includes(letter) && number % 2 === 0)
+    ? "black"
+    : "white";
 }
+
 const cell1 = "A1";
 const cell2 = "C3";
 const res = chessBoardCellColor(cell1, cell2);
