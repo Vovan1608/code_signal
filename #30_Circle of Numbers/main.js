36a30d9dc304a8dc41eb8190b53d84f803b9b924
Vovan1608 <vovan1608@gmail.com>
2021-03-17 14:23:06 +0200

    done 30 task

--- #30_Circle of Numbers/main.js
+++ #30_Circle of Numbers/main.js
@@ -14,7 +14,7 @@ Guaranteed constraints:
  */
 
 function circleOfNumbers(n, firstNumber) {
-  return firstNumber < n / 2 ? n / 2 + firstNumber : firstNumber - n / 2;
+  return (firstNumber + n / 2) % n;
 }
 
 const n = 10;
