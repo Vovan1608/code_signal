09abc432ff5389cf436ddc640b5d58aeba749d61
Vovan1608 <vovan1608@gmail.com>
2021-03-17 14:02:06 +0200

    added task to 30

---
+++ #30_Circle of Numbers/main.js
@@ -0,0 +1,16 @@
+/* 
+Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+Example
+For n = 10 and firstNumber = 2, the output should be
+circleOfNumbers(n, firstNumber) = 7
+[input] integer n
+A positive even integer.
+Guaranteed constraints:
+4 ≤ n ≤ 20.
+[input] integer firstNumber
+Guaranteed constraints:
+0 ≤ firstNumber ≤ n - 1.
+ */
+
+function circleOfNumbers(n, firstNumber) {}
