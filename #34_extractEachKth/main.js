07081bb6fe33d88e7d342e267e393df10109651c
Vovan1608 <vovan1608@gmail.com>
2021-03-18 11:56:22 +0200

    added style

--- #34_extractEachKth/main.js
+++ #34_extractEachKth/main.js
@@ -8,3 +8,18 @@ extractEachKth(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].
 function extractEachKth(inputArray, k) {
   return inputArray.splice(inputArray.findIndex(k), 1);
 }
+
+const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const k = 3;
+
+const res = extractEachKth(inputArray, k);
+
+const add = document.querySelector("#add");
+
+const el = document.createElement("div");
+el.innerText = res;
+el.style = `	background: #61dafb; 
+   	font-size: 36px; 
+   	text-align: center; 
+   	border: 1px solid black
+`;
