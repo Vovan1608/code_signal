d4adc59b48d79edc2b3ec2ce555eb654a3643993
Vovan1608 <vovan1608@gmail.com>
2021-03-18 11:53:01 +0200

    added function extractEachKth

--- #34_extractEachKth/main.js
+++ #34_extractEachKth/main.js
@@ -4,3 +4,7 @@ Example
 For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3, the output should be
 extractEachKth(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].
 */
+
+function extractEachKth(inputArray, k) {
+  return inputArray.splice(inputArray.findIndex(k), 1);
+}
