9e76e0fa04ea0b76e3ef83c5774c0ebc42b24f04
Vovan1608 <vovan1608@gmail.com>
2021-03-20 10:43:56 +0200

    done #39 KnapsackLight

--- #39_Knapsack Light/main.js
+++ #39_Knapsack Light/main.js
@@ -30,6 +30,12 @@ function knapsackLight(value1, weight1, value2, weight2, maxW) {
     return value1 > value2 ? value1 : value2;
   }
   return maxW >= weight1 ? value1 : value2;
+
+  // return Math.max(
+  // 	maxW >= weight1 && value1,
+  // 	maxW >= weight2 && value2,
+  // 	maxW >= weight1 + weight2 && value1 + value2
+  // );
 }
 
 const res1 = knapsackLight(10, 5, 6, 4, 8); // 10
