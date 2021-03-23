a1747d66cbb605ba430240ae593df57bf45b3fbe
Vovan1608 <vovan1608@gmail.com>
2021-03-23 12:22:14 +0200

    added base code

---
+++ #48_isDigit/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/* 
+Determine if the given character is a digit or not.
+Example
+For symbol = '0', the output should be
+isDigit(symbol) = true;
+For symbol = '-', the output should be
+isDigit(symbol) = false.
+*/
+
+function isDigit(symbol) {}
+
+const symbol = "0";
+const res = isDigit(symbol);
+
+const add = document.querySelector("#add");
+add.style = `
+	background-color: #61dafb;
+	font-size: 36px;
+	text-align: center;
+	border: 1px solid black;
+`;
+
+const el = document.createElement("div");
+el.innerText = res;
+
+add.append(el);
