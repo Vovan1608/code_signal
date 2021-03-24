1bd3db16ba8f4ac2eae97092c8c4616dc3bd865d
Vovan1608 <vovan1608@gmail.com>
2021-03-24 13:12:51 +0200

    added base code

---
+++ #54_sumUpNumbers/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/* 
+CodeMaster has just returned from shopping. He scanned the check of the items he bought and gave the resulting string to Ratiorg to figure out the total number of purchased items. Since Ratiorg is a bot he is definitely going to automate it, so he needs a program that sums up all the numbers which appear in the given input.
+Help Ratiorg by writing a function that returns the sum of numbers that appear in the given inputString.
+Example
+For inputString = "2 apples, 12 oranges", the output should be
+sumUpNumbers(inputString) = 14.
+*/
+
+function sumUpNumbers(inputString) {}
+
+const inputString = "2 apples, 12 oranges"; // -> 14
+
+const add = document.querySelector("#add");
+add.style = `
+	background-color: #61dafb;
+	font-size: 36px;
+	text-align: center;
+	border: 2px solid #000;
+`;
+
+const el = document.createElement("div");
+el.innerText = sumUpNumbers(inputString);
+
+add.append(el);
